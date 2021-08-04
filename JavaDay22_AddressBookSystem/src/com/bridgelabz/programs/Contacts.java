5266de9b6e541a94d9854b2a4392e081f4915290
Naveen2706 <naveen271996@gmail.com>
2021-08-04 23:09:34 +0530

    Search by City and State

---
+++ JavaDay22_AddressBookSystem/src/com/bridgelabz/programs/Contacts.java
@@ -0,0 +1,49 @@
+package com.bridgelabz.programs;
+
+public class Contacts {
+	String firstName;
+	String lastName;
+	String address;
+	String city;
+	String state;
+	Long zipCode;
+	Long phoneNumber;
+	String email;
+	public Contacts(String firstName, String lastName, String address, String city, String state, Long zipCode,
+					Long phoneNumber, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zipCode = zipCode;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "Contacts [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+				+ ", state=" + state + ", zipCode=" + zipCode + ", phoneNumber=" + phoneNumber + ", email=" + email
+				+ "]";
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+
+}
